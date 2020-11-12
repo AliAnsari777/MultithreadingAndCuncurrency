17a730d39181703d93f3d6147766c0cc6181c568
ALi Ansari <alidk2013@gmail.com>
2020-11-12 14:44:27 -0700

    Daemon Thread completed

--- src/DaemonThread.java
+++ src/DaemonThread.java
@@ -1,4 +1,32 @@
 
+class childThread implements Runnable{
+    public void run(){
+        while (true){
+            System.out.println("child thread is executing");
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
 
 public class DaemonThread {
+    public static void main(String[] args) throws InterruptedException {
+        Thread child = new Thread(new childThread());
+        child.setDaemon(true);
+        child.start();
+
+        System.out.println("Main thread is running");
+        Thread.sleep(500);
+
+        System.out.println("Main thread is running");
+        Thread.sleep(500);
+
+        System.out.println("Main thread is running");
+        Thread.sleep(500);
+
+        System.out.println("Main thread is done");
+    }
 }
