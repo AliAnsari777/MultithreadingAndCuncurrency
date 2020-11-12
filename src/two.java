59f58d06b04a959a8334d9ad84119a06e8f888be
ALi Ansari <alidk2013@gmail.com>
2020-11-11 23:53:21 -0700

    second practice scheduleing

--- src/two.java
+++ src/two.java
@@ -1,2 +1,34 @@
+
+class vegetable extends Thread{
+    public int vegetable = 0;
+    public static boolean chopping = true;
+
+    public vegetable(String name){
+        this.setName(name);
+    }
+
+    public void run(){
+        while (chopping){
+            System.out.println(this.getName() + " chopped a vegetable");
+            vegetable++;
+        }
+    }
+}
+
 public class two {
+    public static void main(String[] args) throws InterruptedException {
+        vegetable ali = new vegetable("ali");
+        vegetable karim = new vegetable("karim");
+
+        ali.start();
+        karim.start();
+
+        Thread.sleep(1000);
+        vegetable.chopping = false;
+
+        ali.join();
+        karim.join();
+        System.out.println("ali chopped: " + ali.vegetable);
+        System.out.println("Karim chopped:" + karim.vegetable);
+    }
 }
